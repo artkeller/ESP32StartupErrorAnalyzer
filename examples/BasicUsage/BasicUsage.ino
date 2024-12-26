ce3058a9f011879e9215966fada411e4608fd605
artkeller <artkeller@gmx.de>
2024-12-26 05:51:06 +0100

    Update BasicUsage.ino

--- examples/BasicUsage/BasicUsage.ino
+++ examples/BasicUsage/BasicUsage.ino
@@ -3,18 +3,25 @@
 // Bedingungen und Callbacks definieren
 std::vector<ESP32StartupErrorAnalyzer::ErrorCondition> getStartupConditions() {
     return {
-        {[]() { return esp_reset_reason() == ESP_RST_PANIC; }, []() { Serial.println("Panik-Reset erkannt."); }},
-        {[]() { return esp_sleep_get_wakeup_cause() == ESP_SLEEP_WAKEUP_EXT0; }, []() { Serial.println("Wakeup durch externes Signal."); }}
+        {[]() { return esp_reset_reason() == ESP_RST_UNKNOWN; }, []() { Serial.println("Reset-Ursache konnte nicht bestimmt werden."); }},
+        {[]() { return esp_reset_reason() == ESP_RST_POWERON; }, []() { Serial.println("Power-On-Ereignis erkannt."); }},
     };
-}
+} 
+    
+
 
 void setup() {
     Serial.begin(115200);
+    while(!Serial) {};
+    Serial.println("Firmware started: ESP32StartupErrorAnalyzer - BasicUsage");
 
     ESP32StartupErrorAnalyzer analyzer(getStartupConditions());
+
+    Serial.println("Start Analyzer...");
     analyzer.analyze();
+    Serial.println("Ready!");
 }
 
 void loop() {
-    // Hauptlogik
+    // Hauptlogik 
 }
